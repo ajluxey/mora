0b4cb913bc93aa50f544d51c0433fd1bc3452589
ajluxey <v_koralina@list.ru>
2023-01-16 01:06:26 +0300

    add correct implimentation

--- attacks/BP.cpp
+++ attacks/BP.cpp
@@ -5,7 +5,7 @@
 
 
 void print_vector_attack(uint8_t *array) {
-    for(int i = 0; i < 12; i++) {
+    for(int i = 0; i < 16; i++) {
         printf("%02x", array[i]);
     }
     printf("\n");
@@ -37,11 +37,11 @@ void random_N_from_range(uint64_t range, uint64_t N, int flow_index) {
     sprintf(name, "%03d", flow_index);
     printf("%s\n", name);
 
-//    std::ofstream file(name);
-//    if(!file.is_open()) {
-//        printf("Can't open file for flow %d\n", flow_index);
-//        return;
-//    }
+    std::ofstream file(name);
+    if(!file.is_open()) {
+        printf("Can't open file for flow %d\n", flow_index);
+        return;
+    }
 
     uint64_t left_border = 0;
     uint64_t mini_range = ceil(range / N);
@@ -51,25 +51,18 @@ void random_N_from_range(uint64_t range, uint64_t N, int flow_index) {
 
     uint64_t uniq;
     uint8_t sponge_key[] = {0x41, 0x61, 0x42, 0x4f};
-    uint8_t sponge_message_with_mac[12] = {0};
+    uint8_t sponge_message_with_mac[16] = {0};
     uint8_t potential_collision[4] = {0};
+    memset(sponge_message_with_mac + 8, 0x80, 1);
 
     for(uint64_t i = 0; i < N; i++) {
-        printf("\n%d\n", flow_index);
         uniq = left_border + random_from_diapazon(gen) % (range % N);
 
         decimal_to_array(uniq, sponge_message_with_mac);
-        sponge_mac(sponge_message_with_mac, 8, sponge_key, 4, sponge_message_with_mac + 8);
-        print_vector_attack(sponge_message_with_mac);
-        sponge_mac(sponge_message_with_mac, 12, sponge_key, 4, potential_collision);
-        for(int j = 0; j < 4; j++) {
-            printf("%02x", potential_collision[j]);
-        }
-        printf("\n");
-        if(i >= 5) break;
+        sponge_mac(sponge_message_with_mac, 8, sponge_key, 4, sponge_message_with_mac + 12);
+        sponge_mac(sponge_message_with_mac, 16, sponge_key, 4, potential_collision);
 
-//        file.write((char*)potential_collision, 4);
-
+        file.write((char*)potential_collision, 4);
         left_border += mini_range;
     }
 }
@@ -103,7 +96,6 @@ void check_collisions(uint32_t flow_index) {
         }
 
         index++;
-        file.ignore(1);
         file.peek();
     }
     first_meeting_index.clear();
@@ -111,6 +103,8 @@ void check_collisions(uint32_t flow_index) {
 
 
 void restore_message(uint8_t *message, int seed, uint32_t index, uint64_t range, uint64_t N) {
+    memset(message + 8, 0x80, 1);
+
     uint64_t left_border = 0;
     uint64_t mini_range = ceil(range / N);
 
@@ -124,21 +118,19 @@ void restore_message(uint8_t *message, int seed, uint32_t index, uint64_t range,
     left_border += mini_range * (index);
     uniq = left_border + random_from_diapazon(gen) % (range % N);
     decimal_to_array(uniq, message);
-    sponge_mac(message, 8, sponge_key, 4, message + 8);
-    memcpy(message, message, 8);
-    sponge_mac(message, 12, sponge_key, 4, message + 8);
+    sponge_mac(message, 8, sponge_key, 4, message + 12);
 }
 
 
 void generate_two_and_check(uint8_t *message_1, uint8_t *message_2) {
     uint8_t constant_part[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
 
-    uint8_t test_1[22];
-    uint8_t test_2[22];
+    uint8_t test_1[26];
+    uint8_t test_2[26];
     memcpy(test_1, message_1, 12);
-    memcpy(test_1 + 12, constant_part, 10);
+    memcpy(test_1 + 16, constant_part, 10);
     memcpy(test_2, message_2, 12);
-    memcpy(test_2 + 12, constant_part, 10);
+    memcpy(test_2 + 16, constant_part, 10);
 
     uint8_t sponge_key[] = {0x41, 0x61, 0x42, 0x4f};
     uint8_t result_1[4] = {0};
@@ -156,36 +148,13 @@ void generate_two_and_check(uint8_t *message_1, uint8_t *message_2) {
 }
 
 
-bool corrected_restore(uint8_t *restored_message, uint8_t *mac) {
-    uint8_t result[4] = {0};
-    uint8_t key[4] = {0x41, 0x61, 0x42, 0x4f};
-    sponge_mac(restored_message, 12, key, 4, result);
-    printf("file_mac: ");
-    for(int i = 0; i < 4; i++) {
-        printf("%02x", mac[i]);
-    }
-    printf("\nresult_mac: ");
-    for(int i = 0; i < 4; i++) {
-        printf("%02x", result[i]);
-    }
-    printf("\n");
-
-    for(int i = 0; i < 4; i++) {
-        if (mac[i] != result[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
 int attack_BP() {
     uint64_t N = 0xffffffff;
     uint64_t range = 0xffffffffffffffff;
 
 //    // generate mac as: mes -> mac_, mes||mac -> mac
     uint64_t n_for_each_flow = N / FLOWS_COUNT;
-    random_N_from_range(range, n_for_each_flow, 0);
+////    random_N_from_range(range, n_for_each_flow, 0);
 //
 //    std::vector<std::thread> threads;
 //    for(int i = 0; i < 1; i++) {//FLOWS_COUNT; i++) {
@@ -201,23 +170,11 @@ int attack_BP() {
 
     // check attack
     uint64_t index_1 = 0;
-    uint64_t index_2 = 0;
-    uint8_t message_1[12] = {0};
-    uint8_t message_2[12] = {0};
+    uint64_t index_2 = 1;
+    uint8_t message_1[16] = {0};
+    uint8_t message_2[16] = {0};
     restore_message(message_1, 0, index_1, range, n_for_each_flow);
     restore_message(message_2, 0, index_2, range, n_for_each_flow);
-
-    uint8_t mac_1[4];
-    uint8_t mac_2[4];
-    std::ifstream file("000");
-    file.ignore(index_1 * 4);
-    file.read(reinterpret_cast<char *>(mac_1), 4);
-    file.peek();
-    file.ignore((index_2 - index_1 + 1)*4);
-    file.read(reinterpret_cast<char *>(mac_2), 4);
-
-    corrected_restore(message_1, mac_1);
-    corrected_restore(message_2, mac_2);
 
     print_vector_attack(message_1);
     print_vector_attack(message_2);
